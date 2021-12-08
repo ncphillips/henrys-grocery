5475b0a6bc9da6d37d7b2d7fe00a26608631b103
ncphi <nolan@ncphi.com>
2021-12-08 17:11:39 -0400

    Refactor: Extract logic for getting the discount to price

--- app/src/main/java/henrys/grocery/Store.java
+++ app/src/main/java/henrys/grocery/Store.java
@@ -7,15 +7,19 @@ import java.util.ArrayList;
 public class Store {
     ArrayList<Discount> discounts = new ArrayList<>();
 
+    public void addDiscount(Discount discount) {
+        this.discounts.add(discount);
+    }
+
     public Double calculateBasketPrice(Basket basket) {
         Double subtotal = basket.getItemList().stream().map(Product::getPrice).reduce(0.0, Double::sum);
 
-        Double discountToPrice = discounts.stream().map(discount -> discount.calculateTotalForBasket(basket)).reduce(0.0, Double::sum);
+        Double discountToPrice = getDiscountToPrice(basket);
 
         return subtotal - discountToPrice;
     }
 
-    public void addDiscount(Discount discount) {
-        this.discounts.add(discount);
+    private Double getDiscountToPrice(Basket basket) {
+        return discounts.stream().map(discount -> discount.calculateTotalForBasket(basket)).reduce(0.0, Double::sum);
     }
 }
