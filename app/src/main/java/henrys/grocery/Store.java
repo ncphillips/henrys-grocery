84c4b1059ff42892e9afd24a3aa7a6ff09b59b6d
ncphi <nolan@ncphi.com>
2021-12-08 11:33:53 -0400

    Refactor: simplify Store#calculateBasketPrice

--- app/src/main/java/henrys/grocery/Store.java
+++ app/src/main/java/henrys/grocery/Store.java
@@ -2,12 +2,6 @@ package henrys.grocery;
 
 public class Store {
     public Double calculateBasketPrice(Basket basket) {
-        if (basket.getItemList().size() > 1) {
-            return basket.getItemList().stream().map(Product::getPrice).reduce(0.0, Double::sum);
-        }
-        if (basket.getItemList().size() > 0) {
-            return basket.getItemList().get(0).getPrice();
-        }
-        return 0.0;
+        return basket.getItemList().stream().map(Product::getPrice).reduce(0.0, Double::sum);
     }
 }
